ced0fd86d3cf44a6e3607e975ad0b3cd648881ab
syamimiazizudin <syamimi.azizudin@gmail.com>
2018-02-21 14:48:49 +0800

    update saving link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -113,7 +113,7 @@
                         </div>
                     </div>
                 </div> -->
-                <div class="row">
+        <div class="row">
         <div class="col-md-4 col-md-offset-4">
             <div class="panel panel-default">
                 <div class="panel-heading custom-heading">Login</div>
