59c78afe78a3418d45a991f50824ee27d9f83778
syamimiazizudin <syamimi.azizudin@gmail.com>
2018-02-22 12:52:31 +0800

    edit welcome page - login form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -76,8 +76,8 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <!-- <a href="{{ route('login') }}">Login</a> -->
-                        <a href="{{ route('register') }}">Register</a>
+                        {{-- <a href="{{ route('login') }}">Login</a> --}}
+                        {{-- <a href="{{ route('register') }}">Register</a> --}}
                     @endauth
                 </div>
             @endif
@@ -86,96 +86,75 @@
                 <div class="title m-b-md">
                     UMW Cost Saving Initiative
                 </div>
+                @if (Route::has('login'))
+                    @auth
+                        <h2>Welcome {{ Auth::user()->username }}! </h2>
+                    @else
+                        <div class="row">
+                            <div class="col-md-4 col-md-offset-4">
+                            <div class="panel panel-default">
+                                <div class="panel-heading custom-heading">Login</div>
 
-                <!-- <div class="row ">
-                    <div class="col-md-8 col-md-offset-2 padding1">
-                        <div class="col-md-4">
-                            <a type="button" href="{{ url('/dashboard') }}" class="btn btn-lg btn-primary custom">Main Dashboard</a>
-                        </div>
-                        <div class="col-md-4">
-                            <a type="button" href="{{ url('/group-dashboard') }}" class="btn btn-lg btn-primary custom">Group Dashboard</a>
-                        </div>
-                        <div class="col-md-4">
-                            <a type="button" href="{{ url('/user') }}" class="btn btn-lg btn-primary custom">User Management Page</a>
-                        </div>
-                    </div>
-                </div> -->
-                <!-- <div class="row ">
-                    <div class="col-md-8 col-md-offset-2 padding2">
-                        <div class="col-md-4">
-                            <a type="button" href="{{ url('/company') }}" class="btn btn-lg btn-primary custom">Company Management Page</a>
-                        </div>
-                        <div class="col-md-4">
-                            <a type="button" href="{{ url('/initiative') }}" class="btn btn-lg btn-primary custom">Initiative Management Page</a>
-                        </div>
-                        <div class="col-md-4">
-                            <a type="button" href="{{ url('/saving') }}" class="btn btn-lg btn-primary custom">Saving Management Page</a>
-                        </div>
-                    </div>
-                </div> -->
-        <div class="row">
-        <div class="col-md-4 col-md-offset-4">
-            <div class="panel panel-default">
-                <div class="panel-heading custom-heading">Login</div>
+                                <div class="panel-body">
+                                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                                        {{ csrf_field() }}
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
+                                        <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
+                                            <label for="username" class="col-md-4 control-label">Username</label>
 
-                        <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
-                            <label for="username" class="col-md-4 control-label">Username</label>
+                                            <div class="col-md-6">
+                                                <input id="username" type="username" class="form-control" name="username" value="{{ old('username') }}" required autofocus>
 
-                            <div class="col-md-6">
-                                <input id="username" type="username" class="form-control" name="username" value="{{ old('username') }}" required autofocus>
+                                                @if ($errors->has('username'))
+                                                    <span class="help-block">
+                                                        <strong>{{ $errors->first('username') }}</strong>
+                                                    </span>
+                                                @endif
+                                            </div>
+                                        </div>
 
-                                @if ($errors->has('username'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('username') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+                                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                            <label for="password" class="col-md-4 control-label">Password</label>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                                            <div class="col-md-6">
+                                                <input id="password" type="password" class="form-control" name="password" required>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                                @if ($errors->has('password'))
+                                                    <span class="help-block">
+                                                        <strong>{{ $errors->first('password') }}</strong>
+                                                    </span>
+                                                @endif
+                                            </div>
+                                        </div>
 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+                                        <div class="form-group">
+                                            <div class="col-md-6 col-md-offset-4">
+                                                <div class="checkbox">
+                                                    <label>
+                                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
+                                        <div class="form-group">
+                                            <div class="col-md-8 col-md-offset-4">
+                                                <button type="submit" class="btn btn-primary">
+                                                    Login
+                                                </button>
+
+                                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                    Forgot Your Password?
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
                             </div>
                         </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+                    @endauth
+                @endif
             </div>
         </div>
 
