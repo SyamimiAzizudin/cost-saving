e7a012ec51a397fc53211a8313cbd962ddd05485
syamimiazizudin <syamimi.azizudin@gmail.com>
2018-02-22 16:22:47 +0800

    update table saving - fix screen

--- resources/views/saving/company_saving.blade.php
+++ resources/views/saving/company_saving.blade.php
@@ -18,22 +18,22 @@
                     <table class="main-table">
                     <thead>
                         <tr>
-                          <th class="fixed-side">Area</th>
-                          <th class="fixed-side">Analyze Factors Or Causes Contributing To Current Performance</th>
-                          <th class="fixed-side">Proposed Action To Be Taken To Achieve Saving</th>
-                          <th class="fixed-side">Cost Reduction</th>
-                          <th class="col">JAN</th>
-                          <th class="col">FEB</th>
-                          <th class="col">MAR</th>
-                          <th class="col">APR</th>
-                          <th class="col">MAY</th>
-                          <th class="col">JUN</th>
-                          <th class="col">JUL</th>
-                          <th class="col">AUG</th>
-                          <th class="col">SEP</th>
-                          <th class="col">OCT</th>
-                          <th class="col">NOV</th>
-                          <th class="col">DEC</th>
+                          <th class="fixed-side"><b>Area</b></th>
+                          <th class="fixed-side"><b>Analyze Factors Or Causes Contributing To Current Performance</b></th>
+                          <th class="fixed-side"><b>Proposed Action To Be Taken To Achieve Saving</b></th>
+                          <th class="fixed-side"><b>Cost Reduction</b></th>
+                          <th class="col"><b>JAN</b></th>
+                          <th class="col"><b>FEB</b></th>
+                          <th class="col"><b>MAR</b></th>
+                          <th class="col"><b>APR</b></th>
+                          <th class="col"><b>MAY</b></th>
+                          <th class="col"><b>JUN</b></th>
+                          <th class="col"><b>JUL</b></th>
+                          <th class="col"><b>AUG</b></th>
+                          <th class="col"><b>SEP</b></th>
+                          <th class="col"><b>OCT</b></th>
+                          <th class="col"><b>NOV</b></th>
+                          <th class="col"><b>DEC</b></th>
                       </tr>
                     </thead>
                     <tbody>
@@ -42,7 +42,7 @@
                             <th class="fixed-side" rowspan="3">{{ $v->area }}</th>
                             <th class="fixed-side" rowspan="3">{{ $v->analyze }}</th>
                             <th class="fixed-side" rowspan="3">{{ $v->action }}</th>
-                            <th class="fixed-side">Target Cost (RM)</th>
+                            <th class="fixed-side"><b>Target Saving (RM)</b></th>
                             @for($i = 1; $i <= 12; $i++)
                                 @if($company_savings[$v->id][$i]['target_saving'] != null)
                                   <td>{{ $company_savings[$v->id][$i]['target_saving'] }}</td>
@@ -52,7 +52,7 @@
                             @endfor
                         </tr>
                         <tr>
-                            <th class="fixed-side">Actual Cost (RM)</th>
+                            <th class="fixed-side"><b>Actual Saving (RM)</b></th>
                             @for($i = 1; $i <= 12; $i++)
                                 @if($company_savings[$v->id][$i]['actual_saving'] != null)
                                     <td>{{ $company_savings[$v->id][$i]['actual_saving'] }}</td>
@@ -62,11 +62,11 @@
                             @endfor
                         </tr>
                         <tr>
-                            <th class="fixed-side">Percentage %</th>
+                            <th class="fixed-side"><b>Percentage %</b></th>
                             @for($i = 1; $i <= 12; $i++)
                                 @if($company_savings[$v->id][$i]['actual_saving'] != null && $company_savings[$v->id][$i]['target_saving'] != null)
                                     <td>
-                                        {{ number_format($company_savings[$v->id][$i]['actual_saving'] / $company_savings[$v->id][$i]['target_saving'], 2)}}
+                                        {{ number_format(($company_savings[$v->id][$i]['actual_saving'] / $company_savings[$v->id][$i]['target_saving'])*100, 0)}}
                                     </td>
                                 @else
                                     <td></td>
