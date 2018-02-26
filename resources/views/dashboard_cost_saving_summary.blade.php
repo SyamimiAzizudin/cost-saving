b5199bff9888d28ecfc47937f73bf37b5c818675
syamimiazizudin <syamimi.azizudin@gmail.com>
2018-02-26 09:42:10 +0800

    add % dropdown cost saving summary - main and group

--- resources/views/dashboard_cost_saving_summary.blade.php
+++ resources/views/dashboard_cost_saving_summary.blade.php
@@ -11,8 +11,7 @@
             <td><a href="{{ url('/group-dashboard') }}/{{$v->group}}"><label for="company" class="col-md-12 control-label">{{  $v->group }}</label></a></td>
             <td><label for="saving_target" class="col-md-12 number text-right control-label">{{  number_format( ($v->target), 2, '.', ',') }}</label></td>
             <td><label for="actual_saving" class="col-md-12 number text-right control-labe fail">{{  number_format( ($v->actual), 2, '.', ',' ) }}</label></td>
-            <td><label for="Target" class="col-md-12 number text-center control-label">45</label></td>
-            {{-- <td><label class="col-md-2 text-left"><a href="{{ url('/group-dashboard') }}/{{$company->group}}">View More (Company)</a></label></td> --}}
+            <td><label for="Target" class="col-md-12 number text-center control-label">{{ number_format(($v->actual/$v->target) * 100,0) }}</label></td>
         </tr>
         <?php $i++; ?>
     @empty
