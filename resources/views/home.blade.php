f0be077af30beac031056bbf58dd1c81a03c34cb
syamimiazizudin <syamimi.azizudin@gmail.com>
2018-03-05 18:02:55 +0800

    responsive for home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,28 +29,28 @@
         <!-- /.row -->
 
         <!-- Sign In form -->
-        <div class="row ">
+        <div class="row row1">
             <div class="col-md-8 col-md-offset-2 padding1">
-                <div class="col-md-4">
+                <div class="col-md-4 col-sm-4 col-xs-12">
                     <a type="button" href="{{ url('/dashboard') }}" class="btn btn-lg btn-primary custom1">Main Dashboard</a>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-4 col-sm-4 col-xs-12">
                     <a type="button" href="{{ url('/group-dashboard') }}" class="btn btn-lg btn-primary custom1">Group Dashboard</a>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-4 col-sm-4 col-xs-12">
                     <a type="button" href="{{ url('/user') }}" class="btn btn-lg btn-primary custom1">User Management</a>
                 </div>
             </div>
         </div>
-        <div class="row ">
+        <div class="row row1">
             <div class="col-md-8 col-md-offset-2 padding2">
-                <div class="col-md-4">
+                <div class="col-md-4 col-sm-4 col-xs-12">
                     <a type="button" href="{{ url('/company') }}" class="btn btn-lg btn-primary custom1">Company Management</a>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-4 col-sm-4 col-xs-12">
                     <a type="button" href="{{ url('/initiative-company') }}" class="btn btn-lg btn-primary custom1">Initiative Management</a>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-4 col-sm-4 col-xs-12">
                     <a type="button" href="{{ url('/saving-company') }}" class="btn btn-lg btn-primary custom1">Saving Management</a>
                 </div>
             </div>
