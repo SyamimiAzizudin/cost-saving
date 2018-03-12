fa26423300a61ad8a01a99882f90ec01f4abd951
Hassan Abu Bakar <odin88@gmail.com>
2018-03-12 17:29:47 +0800

    add all the middlewares

---
+++ app/Http/Middleware/CheckSubsidiary.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+
+class CheckAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+
+        if( Auth::check() && Auth::user() == 'subsidiary' )
+        {
+            return $next($request);
+        }
+
+        return redirect('/');
+    }
+}
